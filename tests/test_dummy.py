a943a6de77347f6a8cc5503c063b17cc28f776b5
julianSaavedra <saavedrajulian@hotmail.com.ar>
2024-01-20 01:19:39 -0500

    Adding pyproject.toml

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,7 +1,8 @@
 import unittest
-from dummyFile import returnTrue
+from src.model_dummy import returnTrue
+
 
 class DummyTest(unittest.TestCase):
-    
+
     def test_dummy(self):
         self.assertTrue(returnTrue())
